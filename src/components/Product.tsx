2f3c6c0134342891f18e260756daa632edb6e3ee
Ilya Shutikov <DeliasZen@gmail.com>
2022-11-28 14:08:05 +0300

    fix: fixed routes

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -14,8 +14,8 @@ export function Product({product}: ProductProps) {
 
 
   return (
-    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2 bg-gray-50">Product!
-      {  product.title}
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2 bg-gray-50">
+      {product.title}
       <img src={product.image} className="w-1/6" alt={product.title} />
       <p>{product.title}</p>
       <p className="font-bold">{product.price}</p>
